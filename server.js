129e08abfa875e526cec98098b1db0704e6e3781
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-02 15:58:15 -0500

    fix

--- server.js
+++ server.js
@@ -1,3 +1,19 @@
 const express = require("express");
-const bodyparser = require("body-parser");
+const bodyParser = require("body-parser");
+const path = require("path")
 
+const exp = express();
+const PORT = 8080;
+
+// Setup Express data parsing
+exp.use(bodyParser.urlencoded({ extended: true }));
+exp.use(bodyParser.json());
+
+// API routes
+// require("./routing/apiRoutes")(exp);
+// require("./routing/htmlRoutes")(exp);
+
+exp.listen(PORT, (error, results)=>{
+    if (error) throw error;
+    console.log("Listening on PORT: 8080...");
+});
