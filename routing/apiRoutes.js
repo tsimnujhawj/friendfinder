9461e4cbb5ec678348623e81f231a1a1f185b754
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-07 18:32:01 -0500

    fix

--- routing/apiRoutes.js
+++ routing/apiRoutes.js
@@ -11,16 +11,28 @@ module.exports = (app)=>{
     //     res.json(true);
     // })
     app.post("/api/friends", (req, res)=>{
-        friendsObj.push(req.body);
         let userScores = req.body.scores;
         let matchedName = "";
         let matchedPic = "";
+        let totalDiff = 10000;
         console.log(userScores);
+        // loop through the friends list
         for (let i = 0; i < friendsObj.length; i++){
-            
+            let difference = '';
+            // loop through the user's score
+            for (let j = 0; j < userScores.length; j++){
+                // concate the total
+                difference += Math.abs(friendsObj[i].scores[j] - userScores[j])
+            }
+            console.log(difference);
+            if (difference < totalDiff){
+                matchedName = friendsObj[i].name;
+                matchedPic = friendsObj[i].photo;
+            }
         }
-
+        friendsObj.push(req.body);
         console.log("data posted")
+        res.json({status: "Okay", matchedName: matchedName, matchedPic: matchedPic});
     })
 
 }
