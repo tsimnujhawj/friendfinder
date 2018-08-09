1889c15fbe0d1dfed917f5f56c16b767381ea930
tsimnujhawj <tsimnuj.hawj@gmail.com>
2018-08-09 13:05:51 -0500

    updated friends list

--- app/data/friends.js
+++ app/data/friends.js
@@ -77,7 +77,55 @@ let friendsObj = [{
         1,
         1
       ]
-  }
+  },
+  {
+    "name": "Spinner Dog",
+    "photo": "http://www.pawculture.com/uploads/spinnerdog2.jpg",
+    "scores": [
+    4,
+    5,
+    4,
+    4,
+    5,
+    5,
+    4,
+    4,
+    3,
+    5
+    ]
+    },
+    {
+    "name": "Fight Me",
+    "photo": "https://cdn4.littlethings.com/app/uploads/2017/05/funny-dog-names-three-600x600.jpg",
+    "scores": [
+    1,
+    1,
+    1,
+    5,
+    5,
+    5,
+    1,
+    1,
+    5,
+    1
+    ]
+    },
+    {
+    "name": "Fight Me Then",
+    "photo": "https://em.wattpad.com/64f39a4e32258e0fbdeff0d3b65754ec78833f55/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f4975314144644f485165573371413d3d2d32382e313530313234303339343636663739363131373036363336323235322e6a7067?s=fit&w=720&h=720",
+    "scores": [
+    1,
+    1,
+    1,
+    5,
+    5,
+    5,
+    1,
+    1,
+    5,
+    1
+    ]
+    }
 ]
   
 
